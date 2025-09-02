e9142cce3fb57088e9686e9348f116e5a49e44c7
iddadda <iddadda241@gmail.com>
2025-09-02 12:44:28 +0900

    운동 기록 페이징 작업 완료

--- src/main/java/com/green/otd_healthcare/application/exerciseRecord/ExerciseRecordController.java
+++ src/main/java/com/green/otd_healthcare/application/exerciseRecord/ExerciseRecordController.java
@@ -1,15 +1,18 @@
 package com.green.otd_healthcare.application.exerciseRecord;
 
 
+import com.green.otd_healthcare.application.common.model.PagingReq;
+import com.green.otd_healthcare.application.exerciseRecord.model.ExerciseRecordGetRes;
 import com.green.otd_healthcare.application.exerciseRecord.model.ExerciseRecordPostReq;
 import com.green.otd_healthcare.configuration.model.ResultResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -20,10 +23,19 @@ public class ExerciseRecordController {
     private final ExerciseRecordService exerciseRecordService;
 
     @PostMapping("/elog")
-    public ResultResponse<?> saveExerciseRecord(@Valid @RequestBody ExerciseRecordPostReq req) {
+    public ResponseEntity<?> saveExerciseRecord(@Valid @RequestBody ExerciseRecordPostReq req) {
         log.info("req:{}", req);
 
         Long result = exerciseRecordService.saveExerciseRecord(req);
-        return new ResultResponse<>("운동기록 저장 성공", result);
+        return ResponseEntity.ok(result);
+    }
+
+    // 페이징
+    @GetMapping("/elog/list")
+    public ResponseEntity<?> getExerciseLogList(@ModelAttribute PagingReq req) {
+        log.info("req:{}", req);
+        List<ExerciseRecordGetRes> result = exerciseRecordService.getExerciseLogList(req);
+        log.info("exerciseLogList_result:{}", result);
+        return ResponseEntity.ok(result);
     }
 }
--- src/main/java/com/green/otd_healthcare/application/exerciseRecord/ExerciseRecordMapper.java
+++ src/main/java/com/green/otd_healthcare/application/exerciseRecord/ExerciseRecordMapper.java
@@ -1,7 +1,13 @@
 package com.green.otd_healthcare.application.exerciseRecord;
 
+import com.green.otd_healthcare.application.common.model.PagingDto;
+import com.green.otd_healthcare.application.exerciseRecord.model.ExerciseRecordGetRes;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ExerciseRecordMapper {
+
+    List<ExerciseRecordGetRes> findByLimitTo(PagingDto dto);
 }
