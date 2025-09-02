13b9b67e21ce35bf6d8e9ce4db3310f0fc63b623
iddadda <iddadda241@gmail.com>
2025-09-02 12:13:57 +0900

    프로젝트 환경 설정

---
+++ src/main/java/com/green/otd_healthcare/configuration/model/CustomException.java
@@ -0,0 +1,15 @@
+package com.green.otd_healthcare.configuration.model;
+
+public class CustomException extends RuntimeException {
+
+    private final int status;
+
+    public CustomException(String message, int status) {
+        super(message);
+        this.status = status;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+}
